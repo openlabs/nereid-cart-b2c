8f6ea964bdc1b9179ff05b5febd7348a9a629134
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-02-16 18:47:01 +0530

    Bump version to 2.4.0.2dev

--- __tryton__.py
+++ __tryton__.py
@@ -2,7 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 {
     'name': 'Nereid : Shopping Cart B2C',
-    'version': '2.4.0.1',
+    'version': '2.4.0.2dev',
     'author': 'Openlabs Technologies & Consulting (P) Ltd.',
     'email': 'info@openlabs.co.in',
     'website': 'http://www.openlabs.co.in/',
